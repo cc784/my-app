9be006fc57b830409af1f83e6c164f11191b5b61
Cuar_ <1473815385@qq.com>
2020-06-07 18:02:56 +0800

    JSX表达式

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React ,{ Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
+let msg =1
+let isBoy ='男生'
+//使用對象
+const person = {
+  name: "使用對象"
+};   
+// 使用方法
+const format = (msg) => {
+  return '---' + msg + '---';
+}
+function Test(){
+  return(
+    <Fragment>
+    <h1>我就是jsx</h1>
+    <h1>我大概就是jsx</h1>
+    <h1>{1 + 1}</h1>
+    <h1>{'hello world'}</h1>
+    <h1>{msg}</h1>
+    <h1>{isBoy ? "男生" : "女生"}</h1>
+    <h1>{person.name}</h1>
+  <h1>{format(msg)}</h1>
+    </Fragment>
+  )
+}
 
-let element4 =(
-  <div>
-    <span>橫著躺</span>
-    <span>竪著躺</span>
-  </div>
-)
-
-let man = '發熱'
-let element3 =(
-  <div>
-    <h1>今天是否隔離</h1>
-    <h2>{ man == '發熱'? <button>隔離</button>:element4 }</h2>
-  </div>
-)
-
-ReactDOM.render(
-  element3,
-  document.getElementById('root')
-)
+ReactDOM.render(<Test />, document.getElementById('root'));
