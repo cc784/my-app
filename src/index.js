73ad5588c1f82675abbda5d26dee4481f8566cea
Cuar_ <1473815385@qq.com>
2020-06-07 20:18:15 +0800

    state的声明和使用 构造函数中声明

--- src/index.js
+++ src/index.js
@@ -4,27 +4,24 @@ import './index.css';
 
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
-// state的声明和使用
-
+// state的声明和使用 构造方法
+ 
 class Person extends Component{
-  // 1.声明 state
-  state ={
-    date:'2088',
-    msg:'来日方长'
+  constructor(){
+    super();
+    this.state= {
+      data:"1234",
+      msg:'来日方长'
+    }
   }
-  render() {
-    return (
+  render(){
+    return(
       <div>
-        {/* 2 使用state */}
-        <h1>{this.state.date}</h1>
-        <h2>{this.state.msg}</h2>
+        <h1>{this.state.data}</h1>
+        <h2>{this.state.msg} </h2>
       </div>
     )
   }
 }
-/*
-  1.类属性的方式声明
-  2.state 声明
-  3.render中渲染调用
-*/
+
 ReactDOM.render(<Person/>, document.getElementById('root'))
