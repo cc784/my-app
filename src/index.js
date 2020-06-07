f1f1e21cd182965192d5f70124ecd9bfba6de3ff
Cuar_ <1473815385@qq.com>
2020-06-07 18:28:22 +0800

    JSX嵌套JSX  JSX循环

--- src/index.js
+++ src/index.js
@@ -4,28 +4,23 @@ import './index.css';
 
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
-let msg =1
-let isBoy ='男生'
-//使用對象
-const person = {
-  name: "使用對象"
-};   
-// 使用方法
-const format = (msg) => {
-  return '---' + msg + '---';
-}
-function Test(){
-  return(
-    <Fragment>
-    <h1>我就是jsx</h1>
-    <h1>我大概就是jsx</h1>
-    <h1>{1 + 1}</h1>
-    <h1>{'hello world'}</h1>
-    <h1>{msg}</h1>
-    <h1>{isBoy ? "男生" : "女生"}</h1>
-    <h1>{person.name}</h1>
-  <h1>{format(msg)}</h1>
-    </Fragment>
+// JSX嵌套JSX  JSX循环
+const list = ['苹果', '香蕉', '雪梨', '西瓜'];
+const Test = () => {
+  return (
+    <div >
+      {
+        <div>
+          {
+            list.map(function (v) {
+              return (
+                <h1 key={v}>{v}</h1>
+              )
+            })
+          }
+        </div>
+      }
+    </div>
   )
 }
 
