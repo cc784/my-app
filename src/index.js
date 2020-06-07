b1c241ca5e65e69424cdc1a76ae9baa735c4feb5
Cuar_ <1473815385@qq.com>
2020-06-07 20:08:34 +0800

    state的声明和使用 类属性的方式声明

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React ,{ Fragment, Component } from 'react';
+import React ,{  Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
-// 函数式组件
+// state的声明和使用
 
-const Test = () =>{
-  return(
-    <div>这个就是一个简单的函数式组件</div>
-  )
+class Person extends Component{
+  // 1.声明 state
+  state ={
+    date:'2088',
+    msg:'来日方长'
+  }
+  render() {
+    return (
+      <div>
+        {/* 2 使用state */}
+        <h1>{this.state.date}</h1>
+        <h2>{this.state.msg}</h2>
+      </div>
+    )
+  }
 }
-
-ReactDOM.render(<Test/>, document.getElementById('root'))
+/*
+  1.类属性的方式声明
+  2.state 声明
+  3.render中渲染调用
+*/
+ReactDOM.render(<Person/>, document.getElementById('root'))
