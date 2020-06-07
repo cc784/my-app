7cd3598a09dcd1ae4b695ee2cd359c4bafbd1963
Cuar_ <1473815385@qq.com>
2020-06-07 18:39:58 +0800

    JSX创建组件的本质

--- src/index.js
+++ src/index.js
@@ -5,24 +5,34 @@ import './index.css';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
 // JSX嵌套JSX  JSX循环
-const list = ['苹果', '香蕉', '雪梨', '西瓜'];
-const Test = () => {
-  return (
-    <div >
-      {
-        <div>
-          {
-            list.map(function (v) {
-              return (
-                <h1 key={v}>{v}</h1>
-              )
-            })
-          }
-        </div>
-      }
-      <div style={{ color:'yellow', fontSize:"50px"}}>颜色好好看</div>
-    </div>
+
+const parentProps ={
+  className: "redCls",
+  "data-index":100
+}
+
+const Test =() =>{
+  return React.createElement(
+    "div",
+    parentProps,
+    [
+      React.createElement(
+        "span",
+        null,
+        "你们好呀"
+      ),
+      React.createElement(
+        "h1",
+        null,
+        "你们好呀"
+      ),
+      React.createElement(
+        "h2",
+        null,
+        "这就是jsx语法糖"
+      ),
+    ]
   )
 }
 
-ReactDOM.render(<Test />, document.getElementById('root'));
+ReactDOM.render(<Test/>,document.getElementById('root'))
