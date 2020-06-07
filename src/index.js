c2ed71a7d4f9550ac1d7aba4cd4e07463ac8f23a
Cuar_ <1473815385@qq.com>
2020-06-07 16:46:35 +0800

    函數式組件

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//函數式組件
+function Clock(props) {
+  return (
+    <div>
+      <h1>現在時間是{props.date.toLocaleTimeString()}</h1>
+      <h2>在這是副標題</h2>
+    </div>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function run(){
+  ReactDOM.render(
+    <Clock date={new Date()} />,document.querySelector('#root')
+  )
+}
+setInterval(run, 1000);
