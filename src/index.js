58aae6962ce8daa64c32025c32a72e2cf543125f
Cuar_ <1473815385@qq.com>
2020-06-08 13:14:29 +0800

    input 受控
    受控是指受React控制。可以理解为绑定了value或者checked属性的表单就是受控表单。也可以理解为受控表单就是实现了双向绑定。

--- src/index.js
+++ src/index.js
@@ -5,16 +5,29 @@ import './index.css';
 class App extends Component {
   constructor(){
     super();
+    this.state={
+      inpValue:'',
+      isChecked:''
+    }
     this.inp =React.createRef();
   }
   handleClick(){
     this.inp.current.focus();
   }
+  handleChangeValue(e){
+    this.setState({
+      inpValue: e.currentTarget.value
+    })
+  }
+ 
   render(){
     return(
       <div>
         <input type='text' ref={this.inp}></input>
         <button onClick={this.handleClick.bind(this)}>设置获取焦点</button>
+        <input type='text' value={this.state.inpValue} onChange={this.handleChangeValue.bind(this)}></input>
+        {this.state.inpValue}
+    
       </div>
     )
   }
