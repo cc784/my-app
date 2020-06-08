2e31e9f847d7870d7c86d7ec7b032d6fa554f68d
Cuar_ <1473815385@qq.com>
2020-06-08 12:23:44 +0800

    非受控表单 ref

--- src/index.js
+++ src/index.js
@@ -1,41 +1,23 @@
 import React ,{  Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// 1 引入 prop-types
-import PropTypes from 'prop-types'; 
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-//  props默认值
-// 声明一个类组件
-let HomeNav = (props) => {
-  return  <div>
-  <div>标题</div>
-  <div>{props.children}</div>
-</div>
-  
-}
-// 2  指定要求接收的数据格式
-HomeNav.propTypes ={
-  color:PropTypes.string,
-  nums:PropTypes.number
-}
 
-
-
-class Home extends Component {
-  state = {
-    color: "blue",
-    nums:100
+class App extends Component {
+  constructor(){
+    super();
+    this.inp =React.createRef();
   }
-  render() {
-    return (
+  handleClick(){
+    this.inp.current.focus();
+  }
+  render(){
+    return(
       <div>
-        <HomeNav {...this.state}></HomeNav>
-         {/* 这里放动态插入的内容 */}
-         <div>小标题你 这是一个 插槽</div>
+        <input type='text' ref={this.inp}></input>
+        <button onClick={this.handleClick.bind(this)}>设置获取焦点</button>
       </div>
     )
   }
 }
 
-ReactDOM.render(<Home />, document.getElementById('root'))
+ReactDOM.render(<App/> , document.getElementById('root'))
