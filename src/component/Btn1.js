6d8768db6b157c7a8f1f8d11941f34bb23ce3273
Cuar_ <1473815385@qq.com>
2020-06-10 16:35:50 +0800

    拆分拆分action

--- src/component/Btn1.js
+++ src/component/Btn1.js
@@ -1,9 +1,9 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import {connect  } from "react-redux";
-
+import { cartNumAdd } from "../store/actionCreator";
 class Btn1 extends Component{
-    handleClick= () =>{
-        this.props.addNum();
+    handleClick= (unit) =>{
+        this.props.addNum(unit);
         // console.log(props)
     }
     render(){
@@ -12,6 +12,7 @@ class Btn1 extends Component{
         return(
            <div>
                 <button style={{backgroundColor:"red" }}  onClick={this.handleClick}>Btn1</button>
+                <button onClick={()=>this.handleClick(1)} >Btn1-{this.props.btnNum}</button>
                {this.props.btnNum}
            </div>
         );
@@ -26,17 +27,18 @@ const mapStateToProps =(state) =>{
 }
 
 //
+
+
+  
 const mapDispatchToProps = (dispatch) => {
     return {
-      addNum: function () {
-        dispatch(
-          {
-            type: "add",
-            unit: 1
-          }
-        );
+      addNum: function (unit) {
+        // 触发 addNum的时候  这个函数将会 把 通知 告诉到管理员中
+        // 只要出现了dispatch => 肯定会去到管理员内的一个函数中 
+        dispatch(cartNumAdd(unit) );
       }
     }
   }
-const connFunc=connect(mapStateToProps,mapDispatchToProps);
+const connFunc = connect(mapStateToProps,mapDispatchToProps);
+
 export default connFunc(Btn1);
